5375842c2971f22e9cc7e73736156bb5226b09fa
Swetha Selvakumar <127839034+swethaselvakumars@users.noreply.github.com>
2024-09-08 11:01:47 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,26 @@
+public class string {
+    public static void main(String [] args){
+        String a="hello";
+        // StringBuffer stringobj=new StringBuffer();
+        // stringobj.append(a);
+        // stringobj.append(a);
+        StringBuilder buildobj=new StringBuilder();
+        buildobj.append(a);
+
+        // System.out.println(stringobj);
+        // System.out.println(buildobj);
+
+        // String s1="sweetha";   //009
+        // String s3="sweetha";  //009
+
+        // String s2=new String("sweetha");
+
+        String str="sweetha";
+        str="hari";
+        // immutable not in value affted in address
+
+        System.out.println(str);
+
+    }
+}
+
